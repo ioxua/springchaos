8c3afb02b72b580334bd4b76b133830fe01a98a1
Yehoshua Oliveira <ioxua@newgo.com.br>
2020-07-09 19:20:48 -0300

    feat: adds TodoGroup entity

--- src/main/java/com/example/springchaos/todo/Todo.java
+++ src/main/java/com/example/springchaos/todo/Todo.java
@@ -1,17 +1,31 @@
 package com.example.springchaos.todo;
 
 import com.example.springchaos.domain.DomainImpl;
+import com.example.springchaos.todo.group.TodoGroup;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Getter
+@NoArgsConstructor
 
 @Entity
 public class Todo extends DomainImpl {
 
     @Basic
     private String details;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TodoGroup group;
+
+    public Todo(String details, TodoGroup group) {
+        this.details = details;
+        this.group = group;
+        group.addTodo(this);
+    }
 
 }
---
+++ src/main/java/com/example/springchaos/todo/group/TodoGroupRepository.java
@@ -0,0 +1,9 @@
+package com.example.springchaos.todo.group;
+
+import com.example.springchaos.generic.GenericRepositoryWithALotOfLogic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoGroupRepository extends JpaRepository<TodoGroup, Long>, GenericRepositoryWithALotOfLogic<TodoGroup> {
+}
